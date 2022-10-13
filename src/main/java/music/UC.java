7178154f1520a4bfa176542922498fa5c091d31b
Yifan Lyu <lyu.yifa@northeastern.edu>
2022-10-13 13:25:11 -0700

    implement test for reactions

--- src/main/java/music/UC.java
+++ src/main/java/music/UC.java
@@ -10,4 +10,5 @@ public class UC {
     public static final int noMatchDist = 500000;
     public static final int dotThreshold = 5;
     public static final String shapeDBFileName = "shapeDB.dat";
+    public static final int noBid = 1000;
 }
